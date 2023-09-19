31856ec63798d36febfc79fb8c393b91cba715c3
MercedesFR <redondo@icc.uji.es>
2023-09-19 11:09:38 +0200

    version2
    añado lineas

--- dec2bin.py
+++ dec2bin.py
@@ -1,35 +1,35 @@
-Notepad++ v7.9.5 bug-fixes:
+# ------------------------------------------------------
+# Convierte un número decimal positivo a binario usando un 
+#   determinado número de bits.
+# El binario resultante es un string e.g. "101"
+# Se usa la función bin() que transforma e.g. 3 en "0b11".
+# En esta función se quita el "0b" para dejar el "11"
+# ------------------------------------------------------
+def dec2bin(numero_decimal, numero_bits):
+    numero_binario = bin(numero_decimal)
+    if numero_decimal >= 0:
+        numero_binario = numero_binario[2:len(numero_binario)]  # quita el "0b" del principio
+    
+        while len(numero_binario) < numero_bits:      # añade 0's a la izquierda si hace falta
+            numero_binario = "0" + numero_binario
+    else:
+        numero_binario = numero_binario[3:len(numero_binario)] # quita el "-0b" del principio
+        while len(numero_binario) < numero_bits: # añade 1's a la izquierda si hace falta
+            numero_binario = "1" + numero_binario
+    return numero_binario
 
-1.  Fix Notepad++ not starting up due to SaveAsAdmin plugin (for installer package).
-2.  Fix UNC path with quotes handling in folder & file Open/Save dialog.
-3.  Fix file doesn't save as extension type on Windows 7.
+# ----------------------------------------
+# MAIN
+# ----------------------------------------
+if __name__ == "__main__":
+    # Pide al usuario el número a convertir y el número de bits 
+    # Como el resultado de input es de tipo string, se convierte a entero usando int()
+    numero_decimal = int(input("Escribe el número en decimal que quieres convertir: "))
+    numero_bits = int(input("Cuantos bits tendrá el número binario: "))
 
+    # se llama a la función dec2bin() para hacer la conversión
+    numero_binario = dec2bin(numero_decimal, numero_bits)
 
-Notepad++ v7.9.4 Enhancements & bug-fixes:
-
-1.  Upgrade Scintilla from v4.2.0 to v4.4.6.
-2.  Fix DPI scaling broken while dragging Notepad++ into the 2nd monitor regression.
-3.  Add "Find in Projects" feature.
-4.	Fix project workspace changes lost on save cancel issue.
-5.  Fix folder picker not being launched issue because provided directory is invalid.
-6.  Fix network path (UNC path) not working in file dialog.
-7.  Fix shortcut name more than 64 characters makes a menu entry empty name issue.
-8.  Fix extension appending issue in file dialog.
-9.  Fix file not turning to modified state (with Session Manager plugin) bug after its content being changed.
-10. Add an option to mute all sounds in preferences dialog.
-11. Make JSON auto-indent for brace.
-
-More fixes & implementations detail:
-https://notepad-plus-plus.org/downloads/v7.9.5/
-
-
-Included plugins:
-
-1.  NppExport v0.2.9
-2.  Converter 4.2.1
-3.  Mime Tool 2.5
-
-
-Updater (Installer only):
-
-* WinGup (for Notepad++) v5.1.3
+    # Muestra por pantalla el resultado.
+    # Para imprimir un entero es necesario convertirlo a string con str()
+    print("El numero " + str(numero_decimal) + " es " + numero_binario + " en binario con " + str(numero_bits) + " bits.")
